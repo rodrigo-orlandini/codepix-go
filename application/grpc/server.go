a15a62451394022b7cf3fe0a5fc9435428add9ab
esoft41 <rodrigosorlandini@gmail.com>
2023-10-19 13:56:10 -0300

    FEAT: gRPC server

---
+++ application/grpc/server.go
@@ -0,0 +1,28 @@
+package grpc
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/jinzhu/gorm"
+	"google.golang.org/grpc"
+)
+
+func StartGRPCServer(database *gorm.DB, port int) {
+	grpcServer := grpc.NewServer()
+
+	address := fmt.Sprintf("0.0.0:#{port}")
+	listener, listenError := net.Listen("tcp", address)
+
+	if listenError != nil {
+		log.Fatal("It can't start gRPC server.", listenError)
+	}
+
+	log.Printf("gRPC server has been started at port %d", port)
+	serverError := grpcServer.Serve(listener)
+
+	if serverError != nil {
+		log.Fatal("It can't start gRPC server.", listenError)
+	}
+}
