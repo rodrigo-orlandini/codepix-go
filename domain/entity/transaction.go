4a1bc353e1273f41905fdb648c834ced7bc2d564
esoft41 <rodrigosorlandini@gmail.com>
2023-10-18 22:16:25 -0300

    FEAT: Transaction entity

---
+++ domain/entity/transaction.go
@@ -0,0 +1,101 @@
+package entity
+
+import (
+	"errors"
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+const (
+	TransactionPending   string = "pending"
+	TransactionCompleted string = "completed"
+	TransactionError     string = "error"
+	TransactionConfirmed string = "confirmed"
+)
+
+type Transactions struct {
+	Transaction []*Transaction
+}
+
+type Transaction struct {
+	Base              `valid:"required"`
+	AccountFrom       *Account `valid:"-"`
+	Amount            float64  `json:"amount" valid:"notnull"`
+	PixKeyTo          *PixKey  `valid:"-"`
+	Status            string   `json:"status" valid:"notnull"`
+	Description       string   `json:"description" valid:"notnull"`
+	CancelDescription string   `json:"cancel_description" valid:"-"`
+}
+
+func (transaction *Transaction) isValid() error {
+	_, validationError := govalidator.ValidateStruct(transaction)
+
+	if transaction.Amount <= 0 {
+		return errors.New("The amount value must be greater than 0")
+	}
+
+	if transaction.Status != TransactionPending && transaction.Status != TransactionConfirmed && transaction.Status != TransactionCompleted && transaction.Status != TransactionError {
+		return errors.New("Invalid transaction status")
+	}
+
+	if transaction.AccountFrom.ID == transaction.PixKeyTo.ID {
+		return errors.New("The source and destination account can't be the same")
+	}
+
+	if validationError != nil {
+		return validationError
+	}
+
+	return nil
+}
+
+func NewTransaction(accountFrom *Account, amount float64, pixKey *PixKey, description string) (*Transaction, error) {
+	transaction := Transaction{
+		AccountFrom: accountFrom,
+		Amount:      amount,
+		PixKeyTo:    pixKey,
+		Status:      TransactionPending,
+		Description: description,
+	}
+
+	transaction.ID = uuid.NewV4().String()
+	transaction.CreatedAt = time.Now()
+
+	validationError := transaction.isValid()
+
+	if validationError != nil {
+		return nil, validationError
+	}
+
+	return &transaction, nil
+}
+
+func (transaction *Transaction) Complete() error {
+	transaction.Status = TransactionCompleted
+	transaction.UpdatedAt = time.Now()
+
+	validationError := transaction.isValid()
+
+	return validationError
+}
+
+func (transaction *Transaction) Confirm() error {
+	transaction.Status = TransactionConfirmed
+	transaction.UpdatedAt = time.Now()
+
+	validationError := transaction.isValid()
+
+	return validationError
+}
+
+func (transaction *Transaction) Cancel(description string) error {
+	transaction.Status = TransactionError
+	transaction.CancelDescription = description
+	transaction.UpdatedAt = time.Now()
+
+	validationError := transaction.isValid()
+
+	return validationError
+}
